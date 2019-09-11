0acb9b16233a0961ee15f645de319b9036234a13
David Garza <dgarza0413@gmail.com>
2019-09-10 23:20:12 -0500

    palettes displayed on landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from "./components/Palette";
+import PaletteList from "./components/PaletteList";
 import seedColors from "./seedColors";
 import { generatePalette } from "./components/colorHelpers"
 
@@ -9,14 +10,13 @@ class App extends Component {
     console.log(id)
     return seedColors.find(function (palette) {
       console.log(palette.id)
-      console.log(id)
       return palette.id === id;
     });
   }
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>palette</h1>} />
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
         <Route exact path="/palette/:id" render={(routeProps) => (
           <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
         )}
