956a759cb7da6f5436a3361210ead3fd9665ed89
David Garza <dgarza0413@gmail.com>
2020-03-07 21:18:16 -0600

    finish refactor of colorbox css

--- src/App.js
+++ src/App.js
@@ -7,34 +7,40 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./components/ColorHelpers"
 
 class App extends Component {
-  findPalette(id) {
-    console.log(id)
-    return seedColors.find(function (palette) {
-      console.log(palette.id)
-      return palette.id === id;
-    });
-  }
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/palette/:paletteId/:colorId"
-          render={routeProps =>
-            <SingleColorPalette
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.paletteId)
-              )}
-            />}
-        />
-        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors}{...routeProps} />} />
-        <Route exact path="/palette/:id" render={(routeProps) => (
-          <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
-        )}
-        />
-        {/* <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} /> */}
-      </Switch>
-    );
-  }
+    findPalette(id) {
+        console.log(id)
+        return seedColors.find(function (palette) {
+            console.log(palette.id)
+            return palette.id === id;
+        });
+    }
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/palette/:paletteId/:colorId"
+                    render={routeProps =>
+                        <SingleColorPalette
+                            colorId={routeProps.match.params.colorId}
+                            palette={generatePalette(
+                                this.findPalette(routeProps.match.params.paletteId)
+                            )}
+                        />}
+                />
+                <Route exact path="/"
+                    render={routeProps =>
+                        <PaletteList
+                            palettes={seedColors}
+                            {...routeProps} />} />
+                <Route exact path="/palette/:id"
+                    render={(routeProps) => (
+                        <Palette
+                            palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+                    )}
+                />
+                {/* <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} /> */}
+            </Switch>
+        );
+    }
 }
 
 export default App;
